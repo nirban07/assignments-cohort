8de120532d3ff4da4f811ed7766e78f024f4f6ef
nirban07 <nirban012@gmail.com>
2023-12-29 14:00:22 +0530

    sol for week3 2,3

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const assert = require('assert');
 const express = require('express');
+const { copyFileSync } = require('fs');
 const app = express();
 // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
@@ -10,11 +11,29 @@ const app = express();
 // User will be sending in their user id in the header as 'user-id'
 // You have been given a numberOfRequestsForUser object to start off with which
 // clears every one second
-
+const port = 3000;
 let numberOfRequestsForUser = {};
 setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
+
+app.use((req, res, next) => {
+  if (Object.keys(numberOfRequestsForUser).includes(req.headers["user-id"])) {
+    if (numberOfRequestsForUser[req.headers['user-id']] > 3) {
+      res.status(404).send()
+      // next()
+    } else {
+      numberOfRequestsForUser[req.headers['user-id']]++;
+      next()
+    }
+  }
+  else {
+    numberOfRequestsForUser[req.headers['user-id']] = 0;
+    next()
+  }
+  // console.log("request count is now ", numberOfRequestsForUser[req.headers["user-id"]])
+
+})
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
@@ -23,5 +42,9 @@ app.get('/user', function(req, res) {
 app.post('/user', function(req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
+ 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 module.exports = app;
--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -4,23 +4,49 @@ const express = require('express');
 
 const app = express();
 let errorCount = 0;
-
+const port = 3000;
 // You have been given an express server which has a few endpoints.
 // Your task is to
 // 1. Ensure that if there is ever an exception, the end user sees a status code of 404
 // 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
 
-app.get('/user', function(req, res) {
-  throw new Error("User not found");
+
+function errorCountTracker(error, req, res, next) {
+  errorCount++;
+
+  if (error) {
+    return res.status(404).send('Exception has occurred');
+  } else {
+    next();
+  }
+}
+app.get('/user', function (req, res) {
+  try {
+    throw new Error("User not found");
   res.status(200).json({ name: 'john' });
+  } catch (err){
+    next(err)
+  }
+  
 });
 
 app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+  try {
+    res.status(200).json({ msg: 'created dummy user' });
+  } catch (err) {
+    next(err)
+  }
 });
 
 app.get('/errorCount', function(req, res) {
-  res.status(200).json({ errorCount });
+  try { res.status(200).json({ errorCount }); }
+  catch(err) {
+    next(err)
+  }
 });
+app.use(errorCountTracker)
+app.listen(port, () => {
+  console.log("This app is listening on port ", port);
+})
 
 module.exports = app;
