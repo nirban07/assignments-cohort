320c0f2cfea95bfa0483462ec142692bf7133362
nirban07 <nirban012@gmail.com>
2024-01-01 22:57:33 +0530

    added schema for database

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -12,10 +12,21 @@ const AdminSchema = new mongoose.Schema({
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String,
+    description: String,
+    price: Number,
+    imageLink: String,
+    
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
