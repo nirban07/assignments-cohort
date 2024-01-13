847d75268c8a381fce58025169e6c04899a47b1c
nirban07 <nirban012@gmail.com>
2024-01-13 16:10:39 +0530

    added week 5 solutions look-back-later

---
+++ week-5/level-1/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>Nirban</h1>
+        <p>100x dev cohort2 participant</p>
+        <h4>Interests</h4>
+        <ul>
+          <li>Plone</li>
+          <li>Novu</li>
+          <li>Devtron</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
