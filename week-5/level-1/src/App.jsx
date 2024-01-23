1c01531fe0f72db65f6b410c95c71f5ff0ed2f6f
nirban07 <nirban012@gmail.com>
2024-01-23 15:16:54 +0530

    made business card in week-5/level-1

--- week-5/level-1/src/App.jsx
+++ week-5/level-1/src/App.jsx
@@ -3,15 +3,17 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="container">
-        <h1>Nirban</h1>
+      <div className="shadow-lg text-left text-black bg-white p-4 m-4 leading-8 rounded-lg border  ">
+        <h1 className="text-4xl ">Nirban</h1>
         <p>100x dev cohort2 participant</p>
-        <h4>Interests</h4>
+        <h4 className="text-2xl">Interests</h4>
         <ul>
           <li>Plone</li>
           <li>Novu</li>
           <li>Devtron</li>
         </ul>
+        <button className="bg-sky-400 rounded-md p-2 ">LinkedIn</button>
+        <button className="bg-sky-400 rounded-md p-2  ml-10">Twitter</button>
       </div>
     </>
   );
